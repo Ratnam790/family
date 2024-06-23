5692be74e5b6efd56ea0d0c1d7ca6092a8d8138f
Ratnam790 <ratnammunganda1@gmail.com>
2024-06-23 15:24:20 +0530

    message for validation

--- 01-validation.sh
+++ 01-validation.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIME_STAMP=$(date %F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIME_STAMP-$SCRIPT_NAME.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is ....FAILURE"
+        exit 1
+    else
+        echo "$2 is.....SUCCESS"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then 
@@ -9,5 +22,6 @@ else
     echo "you are in root user"
 fi
 
+
 dnf install mysql -y
-echo "successfully installed mysql"
+VALIDATE $? "INSTALLing MYSQL"
